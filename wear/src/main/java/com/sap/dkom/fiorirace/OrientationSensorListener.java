93e2e5a63180f97abd37f57b7a5927c79674a7fc
Tal <tal.stx@gmail.com>
2015-01-31 17:24:39 +0200

    better motion detection then before

--- wear/src/main/java/com/sap/dkom/fiorirace/OrientationSensorListener.java
+++ wear/src/main/java/com/sap/dkom/fiorirace/OrientationSensorListener.java
@@ -3,6 +3,7 @@ package com.sap.dkom.fiorirace;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 
 import java.text.DecimalFormat;
@@ -10,73 +11,91 @@ import java.text.DecimalFormat;
 
 abstract public class OrientationSensorListener implements SensorEventListener {
     private static final String TAG = "Sensor listener";
-    private static final float NS2MS = 1.0f / 1000000.0f;
-    private float timestamp;
-    private float currentAzim;
-    private float currentPitch;
-    private float currentRoll;
-    private boolean moved = false;
+    private final DecimalFormat decimalFormat = new DecimalFormat("#0.000");
+    private final float[] tempCurrent = new float[16];
+    private final float[] angleChange = new float[3];
+    private float[] currentMatrix = null;
+    private boolean movedLeft = false;
+    private boolean movedRight = false;
+
+    protected OrientationSensorListener() {
+    }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    synchronized public void onSensorChanged(SensorEvent event) {
+
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) return;
+
         if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-            if (event.timestamp == 0) {
-                setCurrentValues(event);
-            } else {
-                float dT = (event.timestamp - timestamp) * NS2MS;
-                if (dT > 250) {
-                    timestamp = event.timestamp;
-                    if (moved) {
-                        setCurrentValues(event);
-                        moved = false;
-                    } else {
-                        float dAzim = calcDeltaAzim(event);
-                        float dPitch = calcDeltaPitch(event);
-                        Log.d(TAG, "pitch delta: " + new DecimalFormat("#0.000").format(dPitch));
-                        float dRoll = calcDeltaRoll(event);
-                        if (dPitch > 0.35) {
-                            move("right");
-                        } else if (dPitch < -0.35) {
-                            move("left");
-                        }
-                    }
+            if (currentMatrix == null) {
+                calibrate(event);
+            }
+            calcAngelChange(event);
+            if (angleChange[0] > 0.45 && angleChange[1] < 0) {
+                currentMatrix = tempCurrent.clone();
+                if (!movedLeft) {
+                    moveRight();
+                } else {
+                    unlockFromLeft();
+                }
+            } else if (angleChange[0] < -0.45 && angleChange[1] > 0) {
+                currentMatrix = tempCurrent.clone();
+                if (!movedRight) {
+                    moveLeft();
+                } else {
+                    unlockFromRight();
                 }
             }
         }
     }
 
+    private void unlockFromRight() {
+        Log.d(TAG, "Unlocked from right:\n" +
+                "X: " + decimalFormat.format(angleChange[0]) + "\n" +
+                "Y: " + decimalFormat.format(angleChange[1]));
+        movedRight = false;
+    }
+
+    private void moveLeft() {
+        Log.d(TAG, "Move left, locking:\n" +
+                "X: " + decimalFormat.format(angleChange[0]) + "\n" +
+                "Y: " + decimalFormat.format(angleChange[1]));
+        move("left");
+        movedLeft = true;
+    }
+
+    private void unlockFromLeft() {
+        Log.d(TAG, "Unlocked from left:\n" +
+                "X: " + decimalFormat.format(angleChange[0]) + "\n" +
+                "Y: " + decimalFormat.format(angleChange[1]));
+        movedLeft = false;
+    }
+
+    private void moveRight() {
+        Log.d(TAG, "Move right, locking:\n" +
+                "X: " + decimalFormat.format(angleChange[0]) + "\n" +
+                "Y: " + decimalFormat.format(angleChange[1]));
+        move("right");
+        movedRight = true;
+    }
+
+    private void calcAngelChange(SensorEvent event) {
+        float[] prevMatrix = currentMatrix.clone();
+        SensorManager.getRotationMatrixFromVector(tempCurrent, event.values);
+        SensorManager.getAngleChange(angleChange, tempCurrent, prevMatrix);
+    }
+
+    private void calibrate(SensorEvent event) {
+        currentMatrix = new float[16];
+        SensorManager.getRotationMatrixFromVector(currentMatrix, event.values);
+    }
+
     private void move(String direction) {
         Log.d(TAG, direction);
         handleMovement(direction);
-        moved = true;
     }
 
     protected abstract void handleMovement(String direction);
-
-    private float calcDeltaRoll(SensorEvent event) {
-        float lastRoll = currentRoll;
-        currentRoll = event.values[2];
-        return currentRoll - lastRoll;
-    }
-
-    private float calcDeltaPitch(SensorEvent event) {
-        float lastPitch = currentPitch;
-        currentPitch = event.values[1];
-        return currentPitch - lastPitch;
-    }
-
-    private float calcDeltaAzim(SensorEvent event) {
-        float lastAzim = currentAzim;
-        currentAzim = event.values[0];
-        return currentAzim - lastAzim;
-    }
-
-    private void setCurrentValues(SensorEvent event) {
-        currentAzim = event.values[0];
-        currentPitch = event.values[1];
-        currentRoll = event.values[2];
-    }
-
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
